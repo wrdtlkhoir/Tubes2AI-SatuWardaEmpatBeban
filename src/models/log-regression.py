b09c6d3a057d699d3685fb0ddd49559ef076fd58
ahsuunn <themalique1910@gmail.com>
2025-11-25 10:50:19 +0700

    feat: mini batch gradient ascent

--- src/models/log-regression.py
+++ src/models/log-regression.py
@@ -39,7 +39,7 @@ class LogRegression():
     def calculate_probability(self, sigma):
         return 1 / (1 + pow(math.e, -sigma))
     
-    # Batch Logistic Regression
+    # 1. Batch Logistic Regression
     def iterate_batch(self, epochs=10):
         for epoch in range(epochs):
             grad = np.zeros(len(self.weight))
@@ -56,7 +56,7 @@ class LogRegression():
             for i in range(len(self.weight)):
                 self.weight[i] += self.learning_rate * grad[i]
 
-    # Stochastic Gradient Ascent
+    # 2. Stochastic Gradient Ascent
     # for all set of data randomized the order and iterate by that order until it gets the value 
     def iterate(self, epochs=10):
         for epoch in range(epoch): 
@@ -70,6 +70,30 @@ class LogRegression():
 
                 for i in range(len(self.weight)):
                     self.weight[i] += self.learning_rate * (y - y_pred) * x_with_bias[i]
+
+    #3. Mini batch gradient ascent
+    def iterate_mini_batch(self, batch_size=4, epochs=10):
+        for epoch in range(epoch): 
+            random.shuffle(self.data)
+
+            for i in range(0, len(self.data), batch_size):
+                batch = self.data[i:i+batch_size]
+
+                # Initialize batch gradient
+                grad = np.zeros(len(self.weight))
+
+                for x, y in batch:
+                    x_with_bias = [self.bias] + x
+                    sigma = self.calculate_sigma(x)
+                    y_pred = self.calculate_probability(sigma)
+
+                    # Accumulate gradient
+                    for j in range(len(self.weight)):
+                        grad[j] += (y - y_pred) * x_with_bias[j]
+
+                # Update weight per batch
+                for j in range(self.weight):
+                    self.weight[j] += self.learning_rate * grad[j]
 
     def predict(self, x):
         sigma = self.calculate_sigma(x)
